e03d43b82951822c06aa7873f73d6024e19b9fbd
LeonardBuhl <leonardbuhl@outlook.de>
2023-02-27 22:25:53 +0100

    add framework for castling lul

--- src/modules/Rook.js
+++ src/modules/Rook.js
@@ -3,7 +3,21 @@ import { Piece } from "./Piece";
 class Rook extends Piece {
     constructor(position) {
         let wasMoved = false;
+        let canCastle = true;
         super(position);
+    }
+
+    setPosition(position) {
+        this.currentPosition = position;
+
+        // sets canCastle to false if the piece is moved for the first time
+        if (this.canCastle == true) {
+            this.disable_castling();
+        }
+    }
+
+    disable_castling() {
+        this.canCastle = false;
     }
 
     getLegalMoves() {
